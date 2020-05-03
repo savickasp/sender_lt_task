ab6d7b941f42145e6f7d768050ca9d3a664c59cc
savickasp <savickas.p@gmail.com>
2020-05-03 23:52:13 +0300

    chaching for 60 seconds done

--- app/Http/Controllers/CatSpeciesController.php
+++ app/Http/Controllers/CatSpeciesController.php
@@ -3,28 +3,50 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Str;
 
 class CatSpeciesController extends Controller
 {
+    protected $key;
+
+    public function __construct()
+    {
+        $this->key = 'route_' . Str::slug(\request()->url());
+    }
+
     public function showThreeCats()
     {
+        if (Cache::has($this->key)) {
+            $cats = Cache::get($this->key);
+        } else {
+            $cats = $this->getSelectedCats();
+            Cache::put($this->key, $cats, 60);
+        }
+
+        return view('cats', ['cats' => $cats]);
+    }
+
+    private function getSelectedCats()
+    {
         $allCats = $this->getCatsArray();
-        $count = 2;
 
-        $cat1 = $allCats[rand(0, $count)];
-        $cat2 = $allCats[rand(0, $count)];
+        $count = count($allCats);
 
-        while ($cat2 === $cat1) {
-            $cat2 = $allCats[rand(0, $count)];
+        $cats[1] = $allCats[rand(0, $count)];
+        $cats[2] = $allCats[rand(0, $count)];
+
+        while ($cats[2] === $cats[1]) {
+            $cats[2] = $allCats[rand(0, $count)];
         }
 
-        $cat3 = $allCats[rand(0, $count)];
+        $cats[3] = $allCats[rand(0, $count)];
 
-        while ($cat3 === $cat1 || $cat3 === $cat2) {
-            $cat3 = $allCats[rand(0, $count)];
+        while ($cats[3] === $cats[1] || $cats[3] === $cats[2]) {
+            $cats[3] = $allCats[rand(0, $count)];
         }
 
-        return view('cats', ['cat1' => $cat1, 'cat2' => $cat2, 'cat3' => $cat3]);
+        return $cats;
     }
 
     private function getCatsArray()
